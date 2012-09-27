3355f5f59385e6867b086ea30b46ae5aea05352b
Scott Torborg <scott@cartlogic.com>
2012-09-26 23:03:00 -0700

    version bump -> 2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="maitai",
-      version='0.1',
+      version='0.2',
       description='Handy WSGI Middleware Utilities',
       long_description='',
       classifiers=[
